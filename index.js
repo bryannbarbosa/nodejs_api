be81756492d539e577d51991aaede66947368705
bryannbarbosa <bryannbarbosa@gmail.com>
2017-08-04 04:55:33 -0300

    cross-origin resolved

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send({name: 'Yoshi'});
 });
